4b7e7d3a709265d791ea4e463cc3f8d77151b5f3
DenisMl <denismldn@gmail.com>
2018-02-09 02:33:28 +0300

    maps exposing logic and rendering

--- js/main.js
+++ js/main.js
@@ -1,14 +1,35 @@
-let exposedMap = [];
+let exposedMap1 = [];
+let exposedMap2= [];
 let islandsAmount = 0;
 let timeout = 0;
-const timeoutLength = 100;
+let indicateTimeout = 0;
+const timeoutLength = 150;
+const indicateTimeoutLength = 2;
 
 $(document).ready(function () {
   renderMap(map1, '#map-uncovered-1');
   renderMap(map2, '#map-uncovered-2');
-  islandSearch(map1, '#map-exposed-1');
-  islandSearch(map2, '#map-exposed-2');
+  createEmptyMap(map1, '#map-exposed-1', exposedMap1);
+  createEmptyMap(map2, '#map-exposed-2', exposedMap2);
+
 });
+
+function startFirstMap() {
+  clearTimeouts();
+  islandSearch(map1, '#map-exposed-1', exposedMap1);
+
+}
+
+function startSecondMap() {
+  clearTimeouts();
+  islandSearch(map2, '#map-exposed-2', exposedMap2);
+
+}
+
+function clearTimeouts() {
+  timeout = 0;
+  indicateTimeout = 0;
+}
 
 function renderMap(mapArr, selector) {
   let rowsArr = mapArr.map(function (row, i) {
@@ -31,7 +52,7 @@ function renderEmptyMap(exposedMap, selector) {
   let rowsArr = exposedMap.map(function (row, i) {
     return $(`<div class="map-row row-${i}" ></div>`).append(...renderEmptyMapRow(row, i));
   });
-  $(selector).append(...rowsArr);
+  $(selector).empty().append(...rowsArr);
 }
 
 function renderEmptyMapRow(row, i) {
@@ -40,63 +61,94 @@ function renderEmptyMapRow(row, i) {
   });
 }
 
-function islandSearch(map, selector) {
-  createEmptyMap(map, selector);
+function islandSearch(map, selector, exposedMap) {
+  createEmptyMap(map, selector, exposedMap);
   for (let i = 0; i < map.length; i++) {
     for (let j = 0; j < map[i].length; j++) {
-      if (notChecked(i, j)) {
+      if (notChecked(i, j, exposedMap)) {
+        setTimeout(function () {
+          indicateCurrentSector(i, j, selector);
+        }, timeout);
         if (sectorIsIsland(map, i, j)) {
-          islandsAmount++;
-          discoverIsland(map, i, j, null, null, selector);
+          increaseIslandsAmount(selector);
+          discoverIsland(map, i, j, null, null, selector, exposedMap);
         } else {
-          // debugger;
-          drawWaterSector(i, j, selector)
+          drawWaterSector(i, j, selector, exposedMap)
         }
       }
     }
   }
-  // renderMap(exposedMap, selector);
+  disableBtns();
   console.log(`exposedMap Opened:`);
   console.log(exposedMap);
   console.log(`islandsAmount: ${islandsAmount}`);
   islandsAmount = 0;
 }
 
-function createEmptyMap(map, selector) {
+function disableBtns() {
+  $('.start-btn').prop('disabled', true);
+  setTimeout(function () {
+    enableBtns();
+  }, timeout);
+}
+
+function enableBtns() {
+  $('.start-btn').prop('disabled', false);
+}
+
+function increaseIslandsAmount(selector) {
+  islandsAmount++;
+  if (selector === '#map-exposed-1') {
+    $('.islands-amount1-number').text(islandsAmount);
+  } else {
+    $('.islands-amount2-number').text(islandsAmount);
+  }
+
+
+}
+
+function indicateCurrentSector(i, j, selector) {
+  $(`${selector} .${i}-${j}`).addClass('processing-sector');
+  setTimeout(function () {
+    $(`${selector} .${i}-${j}`).removeClass('processing-sector');
+  }, indicateTimeout);
+  increaseIndicateTimeout();
+}
+
+function createEmptyMap(map, selector, exposedMap) {
   for (let i = 0; i < map.length; i++) {
     exposedMap[i] = new Array(map[0].length);
     for (let j = 0; j < map[i].length; j++) {
       exposedMap[i][j] = null;
     }
   }
-  // console.log(`exposedMap Empty:`);
-  // console.log(exposedMap);
-  // debugger;
   renderEmptyMap(exposedMap, selector);
 }
 
-function notChecked(i, j) {
+function notChecked(i, j, exposedMap) {
   return (exposedMap[i][j] === null)
 }
 
-function discoverIsland(map, i, j, previ, prevj, selector) {
-  if (sectorExists(i, j)) {
-    if (notChecked(i, j)) {
+function discoverIsland(map, i, j, previ, prevj, selector, exposedMap) {
+  if (sectorExists(i, j, exposedMap)) {
+    if (notChecked(i, j, exposedMap)) {
+      setTimeout(function () {
+        indicateCurrentSector(i, j, selector);
+      }, timeout);
       if (sectorIsIsland(map, i, j)) {
-        drawIslandSector(i, j, selector);
+        drawIslandSector(i, j, selector, exposedMap);
         let nextCoords = getNextCoords(i, j, previ, prevj);
         nextCoords.forEach((coord) => {
-          discoverIsland(map, coord[0], coord[1], i, j, selector);
+          discoverIsland(map, coord[0], coord[1], i, j, selector, exposedMap);
         });
       } else {
-        // debugger;
-        drawWaterSector(i, j, selector);
+        drawWaterSector(i, j, selector, exposedMap);
       }
     }
   }
 }
 
-function sectorExists(i, j) {
+function sectorExists(i, j, exposedMap) {
   return (exposedMap[i] !== undefined && exposedMap[i][j] !== undefined);
 }
 
@@ -104,7 +156,7 @@ function sectorIsIsland(map, i, j) {
   return (map[i][j] === 1);
 }
 
-function drawIslandSector(i, j, selector) {
+function drawIslandSector(i, j, selector, exposedMap) {
   exposedMap[i][j] = 1;
   setTimeout(function () {
     renderIslandSector(i, j, selector);
@@ -112,7 +164,7 @@ function drawIslandSector(i, j, selector) {
   increaseTimeout();
 }
 
-function drawWaterSector(i, j, selector) {
+function drawWaterSector(i, j, selector, exposedMap) {
   exposedMap[i][j] = 0;
   setTimeout(function () {
     renderWaterSector(i, j, selector);
@@ -131,6 +183,10 @@ function renderWaterSector(i, j, selector) {
 function increaseTimeout() {
   timeout += timeoutLength;
 }
+
+function increaseIndicateTimeout() {
+  indicateTimeout += indicateTimeoutLength;
+}
 function getNextCoords(i, j, previ, prevj) {
   let nextCoords = [
     [i - 1, j],
@@ -138,12 +194,12 @@ function getNextCoords(i, j, previ, prevj) {
     [i + 1, j],
     [i, j - 1]
   ].filter((coords) => {
-    return (coords[0] !== previ && coords[1] !== prevj);
+    return (!(coords[0] === previ && coords[1] === prevj));
   });
-  console.log(`~~~~~~~~~~~~~~~~~~~~~~~~`);
-  console.log(`curr: [${i}|${j}]`);
-  console.log(`prev: [${previ}|${prevj}]`);
-  console.log(`nextCoords: `);
-  console.dir(nextCoords);
+  // console.log(`~~~~~~~~~~~~~~~~~~~~~~~~`);
+  // console.log(`curr: [${i}|${j}]`);
+  // console.log(`prev: [${previ}|${prevj}]`);
+  // console.log(`nextCoords: `);
+  // console.dir(nextCoords);
   return nextCoords;
 }
